45048da6de304f108057d1cb4c34f5a18bead2b9
kristianp24 <kristianprendi29@gmail.com>
2024-07-10 22:10:19 +0300

    UI Menu Integration

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.weatherforecast
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
    lateinit var text:TextView
+   lateinit var toolbar:androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +27,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         text = findViewById(R.id.infos)
+        toolbar=findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.Madrid->{
+                text.clearComposingText();
+                text.text="Madrid"
+                text.textAlignment= View.TEXT_ALIGNMENT_CENTER
+
+                return true
+            }
+            else->super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
